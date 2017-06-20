1aaef6b8bc677d85b61159d684fefbf35cbec523
Victor Steinmann <victor.steinmann@jiscom.fr>
2017-06-20 09:56:35 +0200

    Changed src of tests to use transpiled files

--- test/tests.js
+++ test/tests.js
@@ -3,7 +3,7 @@
 const it = require('mocha').it;
 const chai = require('chai');
 const expect = require('chai').expect;
-const collect = require('../src');
+const collect = require('../lib');
 const dataset = require('./data');
 
 describe('Collect.js Test Suite', function () {
