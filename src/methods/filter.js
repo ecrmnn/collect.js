919297614e93cdf59d8428d1906e2d0208f534c1
ajay narain mathur <ajaynarainmathur@gmail.com>
2017-06-21 06:31:06 +1000

    added key support and no prodicate funciton passed condition to filter

--- src/methods/filter.js
+++ src/methods/filter.js
@@ -1,9 +1,17 @@
 'use strict';
 
 module.exports = function filter(fn) {
-  const collection = this.items.filter(function(item) {
-    return fn(item);
-  });
+  fn = fn || false;
+
+  let collection = [];
+  for (let i = 0; i < this.items.length; i ++) {
+    const item = this.items[i];
+    if ( fn ) {
+      fn( item, i ) ? collection.push( item ) : null;
+    } else if ( !!item && !( Array.isArray( item ) && !item.length ) ) {
+      collection.push( item );
+    }
+  }
 
   return new this.constructor(collection);
 };
