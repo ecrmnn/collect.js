15cf06b0abb3716613c50574407ef891fe89977d
Carlos Gonzales <carlos.rgn@gmail.com>
2017-06-20 18:37:43 -0600

    group by: allow complex keys

--- src/methods/groupBy.js
+++ src/methods/groupBy.js
@@ -1,23 +1,35 @@
 'use strict';
+const groupByObject = require('group-by-object');
 
-module.exports = function groupBy(key) {
-  let collection = {};
+module.exports = function groupBy(key, complexKeys = false) {
+  if (!complexKeys) {
+    let collection = {};
 
-  this.items.forEach(function(item, index) {
-    let resolvedKey;
+    this.items.forEach(function(item, index) {
+      let resolvedKey;
 
+      if (typeof key === 'function') {
+        resolvedKey = key(item, index);
+      } else {
+        resolvedKey = item[key];
+      }
+
+      if (!collection.hasOwnProperty(resolvedKey)) {
+        collection[resolvedKey] = [];
+      }
+
+      collection[resolvedKey].push(item);
+    });
+
+    return new this.constructor(collection);
+  } else {
+    let fn
     if (typeof key === 'function') {
-      resolvedKey = key(item, index);
+      fn = key
     } else {
-      resolvedKey = item[key];
+      fn = item => item[key]
     }
-
-    if (!collection.hasOwnProperty(resolvedKey)) {
-      collection[resolvedKey] = [];
-    }
-
-    collection[resolvedKey].push(item);
-  });
-
-  return new this.constructor(collection);
+    const map = groupByObject(this.items, fn)
+    return new this.constructor(Array.from(map))
+  }
 };
