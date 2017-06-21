0f96665176a22f33d81199194be593ecd5cdad84
ajay narain mathur <ajaynarainmathur@gmail.com>
2017-06-21 20:39:29 +1000

    updated filter

--- src/methods/filter.js
+++ src/methods/filter.js
@@ -1,9 +1,21 @@
 'use strict';
 
 module.exports = function filter(fn) {
-  const collection = this.items.filter(function(item) {
-    return fn(item);
-  });
+  fn = fn || false;
 
-  return new this.constructor(collection);
+  let filteredArray = [];
+  for (let i = 0; i < this.items.length; i++) {
+    const item = this.items[ i ];
+    if ( fn ) {
+      fn ( item, i ) ? filteredArray.push( item ) : null;
+    } else if ( Array.isArray( item ) ) {
+      item.length ? filteredArray.push( item ) : null;
+    } else if ( item !== undefined && item !== null && typeof item === 'object' ) {
+      Object.keys(item).length ? filteredArray.push( item ) : null;
+    } else if ( item ) {
+      filteredArray.push( item );
+    }
+  }
+
+  return new this.constructor( filteredArray );
 };
