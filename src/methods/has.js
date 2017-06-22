112566bbf1859788309aa5c749047f2a74b0e664
wangzhenhui1992 <605951224@qq.com>
2017-06-22 16:24:36 +0900

    improvement
    improvement

--- src/methods/has.js
+++ src/methods/has.js
@@ -2,7 +2,7 @@
 
 module.exports = function has(key) {
   if (Array.isArray(this.items)) {
-    for (let i = 0; i < this.items.length; i++) {
+    for (let i = 0, length = this.items.length; i < length; i++) {
       if (this.items[i].hasOwnProperty(key)) {
         return true;
       }
