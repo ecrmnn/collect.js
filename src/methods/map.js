1564bd2385b8c0cbe0db54e079cd74408ec607cb
Sebastian Kraft <admin@helfull.de>
2017-06-30 19:40:50 +0200

    fixes another set of eslint errors
    Sorry need to get used to that code style

--- src/methods/map.js
+++ src/methods/map.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports = function map(fn) {
-  if(Array.isArray(this.items)) {
+  if (Array.isArray(this.items)) {
     return new this.constructor(this.items.map(fn));
   }
 
   const hashmap = {};
 
   Object.keys(this.items).forEach((key) => {
-      hashmap[key] = fn(this.items[key]);
+    hashmap[key] = fn(this.items[key]);
   });
 
   return new this.constructor(hashmap);
