960a3d63153fe6810b922b467172ae594819b3c6
ecrmnn <danieleckermann@gmail.com>
2017-07-01 12:39:06 +0200

    Closes #4

--- test/tests.js
+++ test/tests.js
@@ -1872,6 +1872,78 @@ describe('Collect.js Test Suite', function () {
     expect(collectionObject.toArray()).to.eql(collectionObject.values().all());
   });
 
+  it('should append arrays to collection', function () {
+    const expected = [
+      4,
+      5,
+      6,
+      'a',
+      'b',
+      'c',
+      'Jonny',
+      'from',
+      'Laroe',
+      'Jonny',
+      'from',
+      'Laroe'
+    ];
+
+    const firstCollection = collect([4, 5, 6]);
+    const firstArray = ['a', 'b', 'c'];
+    const secondArray = [{
+      who: 'Jonny'
+    }, {
+      preposition: 'from'
+    }, {
+      where: 'Laroe'
+    }];
+    const firstObj = {
+      who: 'Jonny',
+      preposition: 'from',
+      where: 'Laroe'
+    };
+
+    firstCollection.concat(firstArray).concat(secondArray).concat(firstObj);
+
+    expect(firstCollection.count()).to.eql(12);
+    expect(firstCollection.all()).to.eql(expected);
+  });
+
+  it('should append collections to collection', function () {
+    const expected = [
+      4,
+      5,
+      6,
+      'a',
+      'b',
+      'c',
+      'Jonny',
+      'from',
+      'Laroe',
+      'Jonny',
+      'from',
+      'Laroe'
+    ];
+
+    const firstCollection = collect([4, 5, 6]);
+    const secondCollection = collect(['a', 'b', 'c']);
+    const thirdCollection = collect([{
+      who: 'Jonny'
+    }, {
+      preposition: 'from'
+    }, {
+      where: 'Laroe'
+    }]);
+
+    firstCollection
+      .concat(secondCollection)
+      .concat(thirdCollection)
+      .concat(thirdCollection);
+
+    expect(firstCollection.count()).to.eql(12);
+    expect(firstCollection.all()).to.eql(expected);
+  });
+
   it('should be iterable', function () {
     let result = '';
 
