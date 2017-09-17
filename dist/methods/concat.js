91375ff2543dff3e6479c712069ca9eed2f9be60
Ajay Narain Mathur <ajaynarainmathur@gmail.com>
2017-09-17 18:17:11 +1000

    update babel preset from es2015 to preset-env and added yarn lock to lock dependencies

--- dist/methods/concat.js
+++ dist/methods/concat.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
-
 module.exports = function concat(collectionOrArrayOrObject) {
   var _this = this;
 
@@ -9,7 +7,7 @@ module.exports = function concat(collectionOrArrayOrObject) {
 
   if (collectionOrArrayOrObject instanceof this.constructor) {
     list = collectionOrArrayOrObject.all();
-  } else if ((typeof collectionOrArrayOrObject === 'undefined' ? 'undefined' : _typeof(collectionOrArrayOrObject)) === 'object') {
+  } else if (typeof collectionOrArrayOrObject === 'object') {
     list = [];
     Object.keys(collectionOrArrayOrObject).forEach(function (property) {
       list.push(collectionOrArrayOrObject[property]);
@@ -17,7 +15,7 @@ module.exports = function concat(collectionOrArrayOrObject) {
   }
 
   list.forEach(function (item) {
-    if ((typeof item === 'undefined' ? 'undefined' : _typeof(item)) === 'object') {
+    if (typeof item === 'object') {
       Object.keys(item).forEach(function (key) {
         return _this.items.push(item[key]);
       });
