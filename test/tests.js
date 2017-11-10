29e35b404a65548f2e2dfeb44409c5b9acaae1e1
ecrmnn <danieleckermann@gmail.com>
2017-11-10 14:56:54 +0100

    work

--- test/tests.js
+++ test/tests.js
@@ -127,37 +127,7 @@ describe('Collect.js Test Suite', function () {
 
 
 
-  it('should add the given object to the collection. ' +
-    'If the given object contains keys that are already in the collection, ' +
-    'the collections values will be preferred', function () {
-    const collection = collect({
-      a: 'A',
-      b: 'B'
-    });
 
-    const union = collection.union({
-      a: 'AAA',
-      c: 'CCC',
-      b: 'BBB'
-    });
-
-    expect(union.all()).to.eql({
-      a: 'A',
-      b: 'B',
-      c: 'CCC'
-    });
-
-    expect(collection.all()).to.eql({
-      a: 'A',
-      b: 'B'
-    });
-
-    expect(union.all().b).to.eql('B');
-
-    union.forget('b');
-
-    expect(union.all().b).to.eql(undefined);
-  });
 
   it('should merge together the values of the given array with the values of the collection at the corresponding index',
     function () {
