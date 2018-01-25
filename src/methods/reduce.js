5f6cc1be4e14f839186c107edfac35442a7af986
Sebastian Kraft <sebastian.kraft@sauels.de>
2018-01-25 11:39:15 +0100

    adds support for reduce to support hashmaps

--- src/methods/reduce.js
+++ src/methods/reduce.js
@@ -8,10 +8,18 @@ module.exports = function reduce(fn, carry) {
     reduceCarry = carry;
   }
 
-  this.items.forEach((item) => {
-    result = fn(reduceCarry, item);
-    reduceCarry = result;
-  });
+  if (Array.isArray(this.items)) {
+    this.items.forEach(function (item) {
+      result = fn(reduceCarry, item);
+      reduceCarry = result;
+    });
+  } else {
+    const items = this.items;
+    Object.keys(items).forEach(function(key) {
+      result = fn(reduceCarry, items[key], key);
+      reduceCarry = result;
+    });
+  }
 
   return result;
 };
