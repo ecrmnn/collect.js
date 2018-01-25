dd9dc5c69ecd9cc6005b19491155aafc5efb1743
Sebastian Kraft <sebastian.kraft@sauels.de>
2018-01-25 10:28:07 +0100

    code style fix in pull()
    newline before return

--- dist/methods/pull.js
+++ dist/methods/pull.js
@@ -6,5 +6,6 @@ module.exports = function pull(key, defaultValue) {
   var returnValue = this.items[key] || value(defaultValue || null);
 
   delete this.items[key];
+
   return returnValue;
 };
--- src/methods/pull.js
+++ src/methods/pull.js
@@ -6,5 +6,6 @@ module.exports = function pull(key, defaultValue) {
   const returnValue = this.items[key] || value(defaultValue || null);
 
   delete this.items[key];
+
   return returnValue;
 };
