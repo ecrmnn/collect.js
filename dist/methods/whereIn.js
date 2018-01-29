8da3168c26cc35bab448eeb5082d7fe4db905627
Daniël Ducro <info@danield.nl>
2018-01-29 17:11:56 +0100

    accept collections in whereIn and whereNotIn methods - dist

--- dist/methods/whereIn.js
+++ dist/methods/whereIn.js
@@ -1,6 +1,8 @@
 'use strict';
 
 module.exports = function whereIn(key, values) {
+  values = values instanceof this.constructor ? values.all() : values;
+
   var collection = this.items.filter(function (item) {
     return values.indexOf(item[key]) !== -1;
   });
--- dist/methods/whereNotIn.js
+++ dist/methods/whereNotIn.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = function whereNotIn(key, values) {
+  values = values instanceof this.constructor ? values.all() : values;
   var collection = this.items;
 
   values.forEach(function (value) {
