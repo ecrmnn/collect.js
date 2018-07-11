fc4302ba274f40b42d4d8f6569da4531b71fa5dd
Abdullah AlGethami <a.algethami@tamkeentech.sa>
2018-07-11 13:13:57 +0100

    fix count function

--- test/methods/count_test.js
+++ test/methods/count_test.js
@@ -13,4 +13,26 @@ module.exports = (it, expect, collect) => {
     expect(collect({ name: 'Sadio Mané', number: 19 }).count()).to.eql(2);
     expect(collect({ name: 'Sadio Mané', number: 19, club: 'Liverpool FC' }).count()).to.eql(3);
   });
+
+  it('should return the number of items on an array of objects', () => {
+    const array = [];
+    array.name = 'Abdullah AlGethami';
+
+    expect(collect(array).count()).to.eql(1);
+    array.age = '30';
+    expect(collect(array).count()).to.eql(2);
+    array.gender = 'male';
+    expect(collect(array).count()).to.eql(3);
+  });
+
+  it('should return the number of items on mixed values', () => {
+    const array = [1, 2, 3];
+    array.name = 'Abdullah AlGethami';
+
+    expect(collect(array).count()).to.eql(4);
+    array.age = '30';
+    expect(collect(array).count()).to.eql(5);
+    array.gender = 'male';
+    expect(collect(array).count()).to.eql(6);
+  });
 };
