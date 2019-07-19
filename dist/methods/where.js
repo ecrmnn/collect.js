aa2aeea8c49ad016d03a2b501e7c347cade99398
ecrmnn <danieleckermann@gmail.com>
2019-07-19 09:19:11 +0200

    Updates where() to work with as where/1, where/2 and where/3

--- dist/methods/where.js
+++ dist/methods/where.js
@@ -7,12 +7,24 @@ module.exports = function where(key, operator, value) {
   var comparisonOperator = operator;
   var comparisonValue = value;
 
+  var items = values(this.items);
+
+  if (operator === undefined || operator === true) {
+    return new this.constructor(items.filter(function (item) {
+      return nestedValue(item, key);
+    }));
+  }
+
+  if (operator === false) {
+    return new this.constructor(items.filter(function (item) {
+      return !nestedValue(item, key);
+    }));
+  }
+
   if (value === undefined) {
     comparisonValue = operator;
     comparisonOperator = '===';
   }
-
-  var items = values(this.items);
 
   var collection = items.filter(function (item) {
     switch (comparisonOperator) {
--- src/methods/where.js
+++ src/methods/where.js
@@ -7,12 +7,20 @@ module.exports = function where(key, operator, value) {
   let comparisonOperator = operator;
   let comparisonValue = value;
 
+  const items = values(this.items);
+
+  if (operator === undefined || operator === true) {
+    return new this.constructor(items.filter(item => nestedValue(item, key)));
+  }
+
+  if (operator === false) {
+    return new this.constructor(items.filter(item => !nestedValue(item, key)));
+  }
+
   if (value === undefined) {
     comparisonValue = operator;
     comparisonOperator = '===';
   }
-
-  const items = values(this.items);
 
   const collection = items.filter((item) => {
     switch (comparisonOperator) {
