6313ff2595db1fd28c3d41b0deebafa55f356bd0
ecrmnn <danieleckermann@gmail.com>
2019-10-07 13:25:33 +0200

    Merges #231

--- src/methods/toJson.js
+++ src/methods/toJson.js
@@ -1,13 +1,9 @@
 'use strict';
 
-module.exports.toJson = function () {
+module.exports = function toJson() {
   if (typeof this.items === 'object' && !Array.isArray(this.items)) {
     return JSON.stringify(this.all());
   }
 
   return JSON.stringify(this.toArray());
-};
-
-module.exports.toJSON = function () {
-  return this.items;
 };
