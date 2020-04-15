c25bd819f69fe576d6e7af94449c4e274d7d821d
ecrmnn <danieleckermann@gmail.com>
2020-04-15 12:38:45 +0200

    Refectors every() to use Array.prototype.every

--- src/methods/every.js
+++ src/methods/every.js
@@ -5,7 +5,5 @@ const values = require('../helpers/values');
 module.exports = function every(fn) {
   const items = values(this.items);
 
-  return items
-    .map((item, index) => fn(item, index))
-    .indexOf(false) === -1;
+  return items.every(fn);
 };
