c0285fb496123a55aebc27cda546235023c67ee1
ecrmnn <danieleckermann@gmail.com>
2020-05-27 18:15:17 +0200

    Adds tests to isEmpty and where

--- test/methods/isEmpty_test.js
+++ test/methods/isEmpty_test.js
@@ -3,10 +3,18 @@
 module.exports = (it, expect, collect) => {
   it('should return true if collection is empty', () => {
     expect(collect().isEmpty()).to.eql(true);
+
+    expect(collect(null).isEmpty()).to.eql(true);
+    expect(collect(undefined).isEmpty()).to.eql(true);
+
     expect(collect([]).isEmpty()).to.eql(true);
     expect(collect({}).isEmpty()).to.eql(true);
+
     expect(collect(['']).isEmpty()).to.eql(false);
     expect(collect([1, 2, 3]).isEmpty()).to.eql(false);
+
+    expect(collect([null]).isEmpty()).to.eql(false);
+    expect(collect([undefined]).isEmpty()).to.eql(false);
   });
 
   it('should return false if collection is not empty', () => {
