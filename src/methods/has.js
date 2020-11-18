d60526c76c53da644bfd3037a9a0422e3ce488e9
ecrmnn <danieleckermann@gmail.com>
2020-11-18 14:11:51 +0100

    Uses hasOwnProperty in has()

--- src/methods/has.js
+++ src/methods/has.js
@@ -5,6 +5,6 @@ const variadic = require('../helpers/variadic');
 module.exports = function has(...args) {
   const properties = variadic(args);
 
-  return properties.filter(key => Object.keys(this.items).includes(key)).length
+  return properties.filter(key => Object.hasOwnProperty.call(this.items, key)).length
     === properties.length;
 };
