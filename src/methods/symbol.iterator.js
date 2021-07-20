9f8d7d80587c241b7fa2cb288dfdbd3e7c82d542
Rifqi <rifweb.android@gmail.com>
2021-07-20 12:08:40 +0700

    Fixing Symbol iterator to accomodate object item

--- src/methods/symbol.iterator.js
+++ src/methods/symbol.iterator.js
@@ -1,16 +1,30 @@
 'use strict';
 
 module.exports = function SymbolIterator() {
-  let index = -1;
-
-  return {
-    next: () => {
-      index += 1;
-
-      return {
-        value: this.items[index],
-        done: index >= this.items.length,
-      };
-    },
-  };
+  if (Array.isArray(this.items)) {
+    let index = -1;
+    return {
+      next: () => {
+        index += 1;
+        return {
+          value: this.items[index],
+          done: index >= this.items.length,
+        };
+      },
+    };
+  } else if (typeof this.items === 'object') {
+    const keys = Object.keys(this.items);
+    let index = -1;
+    return {
+      next: () => {
+        index += 1;
+        return {
+          value: this.items[keys[index]],
+          done: index >= keys.length,
+        };
+      },
+    };
+  } else {
+    throw new TypeError("Unknown item data type");
+  }
 };
