f2f42bb45f9d224721a1be581d5ab60b27cabb83
SDIjeremy <115488848+SDIjeremy@users.noreply.github.com>
2023-01-21 18:16:09 -0500

    `groupBy` for Objects
    Closes #313

--- src/methods/groupBy.js
+++ src/methods/groupBy.js
@@ -5,24 +5,44 @@ const { isFunction } = require('../helpers/is');
 
 module.exports = function groupBy(key) {
   const collection = {};
+  
+  if (Array.isArray(this.items)) {
+    this.items.forEach((item, index) => {
+      let resolvedKey;
 
-  this.items.forEach((item, index) => {
-    let resolvedKey;
+      if (isFunction(key)) {
+        resolvedKey = key(item, index);
+      } else if (nestedValue(item, key) || nestedValue(item, key) === 0) {
+        resolvedKey = nestedValue(item, key);
+      } else {
+        resolvedKey = '';
+      }
 
-    if (isFunction(key)) {
-      resolvedKey = key(item, index);
-    } else if (nestedValue(item, key) || nestedValue(item, key) === 0) {
-      resolvedKey = nestedValue(item, key);
-    } else {
-      resolvedKey = '';
-    }
+      if (collection[resolvedKey] === undefined) {
+        collection[resolvedKey] = new this.constructor([]);
+      }
 
-    if (collection[resolvedKey] === undefined) {
-      collection[resolvedKey] = new this.constructor([]);
-    }
-
-    collection[resolvedKey].push(item);
-  });
+      collection[resolvedKey].push(item);
+    });
+  } else {
+    Object.keys(this.items).forEach((index) => {
+      let resolvedKey;
+      
+      if (isFunction(key)) {
+        resolvedKey = key(this.items[index], index);
+      } else if (nestedValue(this.items[index], key) || nestedValue(this.items[index], key) === 0) {
+        resolvedKey = nestedValue(this.items[index], key);
+      } else {
+        resolvedKey = '';
+      }
+      
+      if (collection[resolvedKey] === undefined) {
+        collection[resolvedKey] = new this.constructor([]);
+      }
+      
+      collection[resolvedKey].push(this.items[index]);
+    });
+  }
 
   return new this.constructor(collection);
 };
