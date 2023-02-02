e8db90a165ff96bd0be27343cdf14b23a0b0325e
Jeremy Holstein <jjjrmy@gmail.com>
2023-02-01 21:40:32 -0500

    Update hasAny.js

--- dist/methods/hasAny.js
+++ dist/methods/hasAny.js
@@ -1,11 +1,14 @@
 'use strict';
 
 var variadic = require('../helpers/variadic');
+
 module.exports = function hasAny() {
   var _this = this;
+
   for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
     args[_key] = arguments[_key];
   }
+  
   var properties = variadic(args);
   return properties.some(function (key) {
     return Object.hasOwnProperty.call(_this.items, key);
